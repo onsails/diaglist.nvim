dee2f59a45f72fb2ba0dd29cb06ed5357097ee07
Andrey Kuznetsov <fear@loathing.in>
2021-07-21 23:03:22 +0300

    quotes

--- lua/vimway-lsp-diag/loclist.lua
+++ lua/vimway-lsp-diag/loclist.lua
@@ -4,18 +4,18 @@ local M = {
 
 function M.lsp_diagnostics_hook()
   -- FIXME: check foreign loclist
-  local errors = vim.lsp.diagnostic.get_count(0, "Error")
-  local warnings = vim.lsp.diagnostic.get_count(0, "Warning")
+  local errors = vim.lsp.diagnostic.get_count(0, 'Error')
+  local warnings = vim.lsp.diagnostic.get_count(0, 'Warning')
 
   if warnings + errors > 0 then
     vim.lsp.diagnostic.set_loclist({
       open_loclist = false,
-      severity_limit = "Warning",
+      severity_limit = 'Warning',
     })
   else
     -- print('no')
-    -- if vim.fn.win_gettype() == "" then
-    --   vim.cmd("silent! lclose")
+    -- if vim.fn.win_gettype() == '' then
+    --   vim.cmd('silent! lclose')
     -- end
   end
 end
